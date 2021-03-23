04a749481b9e1e9d13390c5df19a60d96e8e15b4
Tris666w <55093987+Tris666w@users.noreply.github.com>
2021-03-23 10:00:54 +0100

    Added SLD2_Mixer

--- Engine/Minigin/SoundSystems.cpp
+++ Engine/Minigin/SoundSystems.cpp
@@ -1,14 +1,27 @@
 #include "MiniginPCH.h"
 #include "SoundSystems.h"
 #include <iostream>
+#include <SDL_mixer.h>
 
 SDLMixerSoundSystem::SDLMixerSoundSystem() : SoundSystem()
 {
+
+	if ( SDL_Init(SDL_INIT_AUDIO) < 0 )
+	{
+		std::cerr << "Core::Initialize( ), error when calling SDL_Init: " << SDL_GetError( ) << std::endl;
+		return;
+	}
+	
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	{
+		std::cerr << "Core::Initialize( ), error when calling Mix_OpenAudio: " << Mix_GetError() << std::endl;
+		return;
+	}
 }
 
 SDLMixerSoundSystem::~SDLMixerSoundSystem()
 {
-	
+	Mix_Quit();
 }
 
 void SDLMixerSoundSystem::Play(const sound_id id, const float volume)
